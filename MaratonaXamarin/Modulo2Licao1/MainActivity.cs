93342ea8b631cbc1d998d50532af0d1076bc27a8
brian.souza <brian.souza@PROJSAPDEV>
2017-02-16 15:51:23 -0200

    Criando exemplo do modulo 2 lição 1

---
+++ MaratonaXamarin/Modulo2Licao1/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Modulo2Licao1
+{
+    [Activity(Label = "Modulo2Licao1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
