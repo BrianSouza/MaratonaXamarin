d5cc8d873b4abd37ded4a53f097949c0e655ae38
brian.souza <brian.souza@PROJSAPDEV>
2017-02-17 17:31:20 -0200

    criando método de conversão, DROID , Módulo 2

--- MaratonaXamarin/Modulo2Licao1/MainActivity.cs
+++ MaratonaXamarin/Modulo2Licao1/MainActivity.cs
@@ -7,12 +7,33 @@ namespace Modulo2Licao1
     [Activity(Label = "Modulo2Licao1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        Button btnConverter = null;
+        EditText txtDolar = null;
+        EditText txtPeso = null;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            this.SetContentView(Resource.Layout.Main);
+            btnConverter = this.FindViewById<Button>(Resource.Id.btnConverter);
+            txtDolar = this.FindViewById<EditText>(Resource.Id.txtDolares);
+            txtPeso = this.FindViewById<EditText>(Resource.Id.txtPesos);
+
+            btnConverter.Click += BtnConverter_Click;
+        }
+
+        private void BtnConverter_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                double dolar = double.Parse(txtDolar.Text);
+                double peso = dolar * 19.5;
+                txtPeso.Text = peso.ToString();
+            }
+            catch (System.Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Short);
+            }
         }
     }
 }
