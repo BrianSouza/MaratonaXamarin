c328d58a91ab22336990bca5e72492a5c66180fb
Brian Souza <brian.diego.souza@gmail.com>
2017-02-20 21:08:00 -0300

    Módulo 3 lição 1 concluida

---
+++ MaratonaXamarin/Modulo3Licao1/ViewController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using UIKit;
+
+namespace Modulo3Licao1
+{
+    public partial class ViewController : UIViewController
+    {
+
+        double conta = 0;
+        double banco = 0;
+        double contaAPagar = 0;
+        double pagoACredito = 0;
+        double pagoFornecedores = 0;
+        double renda = 0;
+        double capital = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            btnCalcular.TouchUpInside += BtnCalcular_TouchUpInside;
+        }
+
+        private void BtnCalcular_TouchUpInside(object sender, EventArgs e)
+        {
+            try
+            {
+                conta = double.Parse(txtConta.Text);
+                banco = double.Parse(txtBanco.Text);
+                contaAPagar = double.Parse(txtValor.Text);
+                pagoACredito = double.Parse(txtPgCredito.Text);
+                pagoFornecedores = double.Parse(txtPgCredito.Text);
+                renda = double.Parse(txtRenda.Text);
+
+                capital = (conta + banco + contaAPagar) - (pagoACredito + pagoFornecedores + renda);
+
+                txtCC.Text = capital.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox("Erro", ex.Message);
+            }
+            
+        }
+
+        private void MessageBox(string v, string message)
+        {
+            using (UIAlertView alerta = new UIAlertView())
+            {
+                alerta.Title = v;
+                alerta.Message = message;
+                alerta.AddButton("Aceitar");
+                alerta.Show();
+            }
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ MaratonaXamarin/Modulo3Licao1/ViewController.designer.cs
@@ -0,0 +1,91 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Modulo3Licao1
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnCalcular { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtBanco { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtCC { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtConta { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPgCredito { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPGForn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtRenda { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtValor { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnCalcular != null) {
+                btnCalcular.Dispose ();
+                btnCalcular = null;
+            }
+
+            if (txtBanco != null) {
+                txtBanco.Dispose ();
+                txtBanco = null;
+            }
+
+            if (txtCC != null) {
+                txtCC.Dispose ();
+                txtCC = null;
+            }
+
+            if (txtConta != null) {
+                txtConta.Dispose ();
+                txtConta = null;
+            }
+
+            if (txtPgCredito != null) {
+                txtPgCredito.Dispose ();
+                txtPgCredito = null;
+            }
+
+            if (txtPGForn != null) {
+                txtPGForn.Dispose ();
+                txtPGForn = null;
+            }
+
+            if (txtRenda != null) {
+                txtRenda.Dispose ();
+                txtRenda = null;
+            }
+
+            if (txtValor != null) {
+                txtValor.Dispose ();
+                txtValor = null;
+            }
+        }
+    }
+}
