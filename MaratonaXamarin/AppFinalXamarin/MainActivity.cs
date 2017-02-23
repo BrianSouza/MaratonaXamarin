5756dc47a3ebd620999231c54d792716a8eed2e7
briansouza <brian-desouza@hotmail.com>
2017-02-23 17:03:55 -0300

    App final droid, criando tool menu

---
+++ MaratonaXamarin/AppFinalXamarin/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace AppFinalXamarin
+{
+    [Activity(Label = "AppFinalXamarin", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "My AppCompat Toolbar";
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+
+    }
+}
+
